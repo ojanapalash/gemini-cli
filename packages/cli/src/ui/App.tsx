f65791bb3c66fce26946cbdbc4ab647835b71824
Abhi <43648792+abhipatel12@users.noreply.github.com>
2025-07-17 19:40:36 -0400

    Migrate /corgi (#4419)

--- packages/cli/src/ui/App.tsx
+++ packages/cli/src/ui/App.tsx
@@ -468,7 +468,6 @@ const App = ({ config, settings, startupWarnings = [], version }: AppProps) => {
   } = useSlashCommandProcessor(
     config,
     settings,
-    history,
     addItem,
     clearItems,
     loadHistory,
